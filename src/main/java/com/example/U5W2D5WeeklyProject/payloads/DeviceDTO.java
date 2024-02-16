83d1311358734e66c2490f69be3df8880f4fb1bb
Daniele Cagnoni <145670818+Sneakydan7@users.noreply.github.com>
2024-02-16 12:31:43 +0100

    Added DTOs

---
+++ src/main/java/com/example/U5W2D5WeeklyProject/payloads/DeviceDTO.java
@@ -0,0 +1,14 @@
+package com.example.U5W2D5WeeklyProject.payloads;
+
+import com.example.U5W2D5WeeklyProject.enums.DeviceType;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class DeviceDTO {
+    @NotEmpty(message = "A device type is required") DeviceType deviceType;
+}
---
+++ src/main/java/com/example/U5W2D5WeeklyProject/payloads/EmployeeDTO.java
@@ -0,0 +1,29 @@
+package com.example.U5W2D5WeeklyProject.payloads;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class EmployeeDTO {
+    @NotEmpty(message = "Name is required")
+    @Size(min = 1, max = 40, message = "Characters must be between 1 and 40")
+    String name;
+    @NotEmpty(message = "Surname is required")
+    @Size(min = 1, max = 40, message = "Characters must be between 1 and 40")
+    String surname;
+
+    @Email(message = "Email is required")
+    String email;
+
+    @NotEmpty(message = "Username is required")
+    @Size(min = 1, max = 40, message = "Characters must be between 1 and 40")
+    String username;
+
+
+}
